78cba61598cf84a04d77d93d3651a20ca8a2b6d8
Meg <kjfs293@gmail.com>
2025-06-07 19:00:07 -0400

    added \n to break lines

--- file_creator.py
+++ file_creator.py
@@ -1,5 +1,5 @@
 name = input("What's your name? \n")
 color = input("What's your favorite color? \n")
 with open('example.txt', 'w') as file:
-    file.write(f"{name} created this file")
-    file.write(f"{color} is my favorite color")
+    file.write(f"{name} created this file. \n")
+    file.write(f"{color} is my favorite color. \n")
